11729bbbd895445160c217c33a942ff6ddf92909
motosharpley <motosharpley@gmail.com>
2018-04-04 15:46:36 -0500

    set for gh pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task("sass", function() {
   return gulp
     .src(["node_modules/bootstrap/scss/bootstrap.scss", "docs/scss/*.scss"])
     .pipe(sass())
-    .pipe(gulp.dest("docs/css"))
+    .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
 
@@ -19,35 +19,35 @@ gulp.task("js", function() {
       "node_modules/jquery/dist/jquery.min.js",
       "node_modules/popper.js/dist/umd/popper.min.js"
     ])
-    .pipe(gulp.dest("docs/js"))
+    .pipe(gulp.dest("src/js"))
     .pipe(browserSync.stream());
 });
 
 // Watch Sass & Serve
 gulp.task("serve", ["sass"], function() {
   browserSync.init({
-    server: "./docs"
+    server: "./src"
   });
 
   gulp.watch(
     ["node_modules/bootstrap/scss/bootstrap.scss", "docs/scss/*.scss"],
     ["sass"]
   );
-  gulp.watch("docs/*.html").on("change", browserSync.reload);
+  gulp.watch("/*.html").on("change", browserSync.reload);
 });
 
 // Move Fonts to docs/fonts
 gulp.task("fonts", function() {
   return gulp
     .src("node_modules/font-awesome/fonts/*")
-    .pipe(gulp.dest("docs/fonts"));
+    .pipe(gulp.dest("src/fonts"));
 });
 
 // Move Font Awesome CSS to docs/css
 gulp.task("fa", function() {
   return gulp
     .src("node_modules/font-awesome/css/font-awesome.min.css")
-    .pipe(gulp.dest("docs/css"));
+    .pipe(gulp.dest("src/css"));
 });
 
 gulp.task("default", ["js", "serve", "fa", "fonts"]);
