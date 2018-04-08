ad242a8bf9c01dbbebc5679ab2ecd2e0fc98a9d6
motosharpley <motosharpley@gmail.com>
2018-04-08 15:32:13 -0500

    clean dest paths

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task("sass", function() {
   return gulp
     .src(["node_modules/bootstrap/scss/bootstrap.scss", "./scss/*.scss"])
     .pipe(sass())
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("css"))
     .pipe(browserSync.stream());
 });
 
@@ -19,7 +19,7 @@ gulp.task("js", function() {
       "node_modules/jquery/dist/jquery.min.js",
       "node_modules/popper.js/dist/umd/popper.min.js"
     ])
-    .pipe(gulp.dest("./js"))
+    .pipe(gulp.dest("js"))
     .pipe(browserSync.stream());
 });
 
@@ -40,14 +40,14 @@ gulp.task("serve", ["sass"], function() {
 gulp.task("fonts", function() {
   return gulp
     .src("node_modules/font-awesome/fonts/*")
-    .pipe(gulp.dest("./fonts"));
+    .pipe(gulp.dest("fonts"));
 });
 
 // Move Font Awesome CSS to /css
 gulp.task("fa", function() {
   return gulp
     .src("node_modules/font-awesome/css/font-awesome.min.css")
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("css"));
 });
 
 gulp.task("default", ["js", "serve", "fa", "fonts"]);
